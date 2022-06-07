13a03ab474ab6db286927b072d4f0129cd980d3f
Emma Janani <99275087+EmmaJanani13@users.noreply.github.com>
2022-06-07 00:49:38 +0000

    Added a button to menu scene

--- js/gameScene.js
+++ js/gameScene.js
@@ -4,20 +4,17 @@
 //
 // Modified by: Emma Janani
 // Created on: June 2022
-// This is the Menu Scene
+// This is the Game Scene
 
 /** 
-  * This class is the Menu Scene.
+  * This class is the Game Scene.
   */
-class MenuScene extends Phaser.Scene {
+class GameScene extends Phaser.Scene {
    /**
     * This method is the constructor. 
     */
   constructor () {
-    super({ key: 'menuScene' })
-
-    this.menuSceneBackgroundImage = null
-    this.startButton = null
+    super({ key: "gameScene" })
   }
 
   /**
@@ -35,9 +32,7 @@ class MenuScene extends Phaser.Scene {
    * Use it to load assets.
    */
   preload() {
-    console.log('Menu Scene')
-    this.load.image('menuSceneBackground', '../images/on.jpg')
-    this.load.image('startButton', '..images/unclicked.png')
+    console.log("Game Scene")
   }
 
   /**
@@ -46,13 +41,6 @@ class MenuScene extends Phaser.Scene {
    * @param {object} data - Any data passed via ScenePlugin.add() or ScenePlugin.start().
    */
   create (data) {
-    this.menuSceneBackgroundImage = this.add.sprite(0, 0, 'menuSceneBackground')
-    this.menuSceneBackgroundImage.x = 1920 / 2
-    this.menuSceneBackgroundImage.y = 1080 / 2
-
-    this.startButton = this.add.sprite(1920 / 2, (1080 / 2) + 100, 'startButton')
-    this.startButton.setInteractive({ useHandCursor: true })
-    this.startButton.on('pointerdown', () => this.clickButton())
   }
 
   /** 
@@ -62,12 +50,9 @@ class MenuScene extends Phaser.Scene {
    *  @param {number} delta - The delta time in ms since the last frame.
    */
   update (time, delta) {
-  }
-
-  clickButton () {
-    this.scene.start('gameScene')
+    // pass
   }
 }
 
-export default MenuScene
+export default GameScene
   
